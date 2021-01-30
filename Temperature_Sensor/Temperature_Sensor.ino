9f9c7723d0867ea8e9c74ffebc4f0a1e9700d780
Hridyakp4 <74726731+Hridyakp4@users.noreply.github.com>
2021-01-30 09:28:52 +0530

    Add files via upload
    Using Arduino

---
+++ Temperature_Sensor/Temperature_Sensor.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Adafruit MLX90614 test");  
+  mlx.begin();  
+}
+
+void loop() {
+  Serial.print("Ambient = "); Serial.print(mlx.readAmbientTempC()); 
+  Serial.print("*C\tObject = "); Serial.print(mlx.readObjectTempC()); Serial.println("*C");
+  if(mlx.readObjectTempC()<=30)
+  {
+  }
+  else
+  {
+  }
+  }
+  Serial.println();
+  delay(500);
+}
